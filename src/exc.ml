6f7c2c82f4703f7d8b56952bb767895be0778394
ruess <ruess>
2001-02-20 23:40:14 +0000

    *** empty log message ***

---
+++ src/exc.ml
@@ -0,0 +1,10 @@
+   
+(*s The following exceptions are raised by any solver which detects an 
+    inconsistent system or an obvious validity.
+ *)
+    
+exception Inconsistent of string
+  
+exception Valid
+
+
