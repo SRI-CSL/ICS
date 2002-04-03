575acfe2c12bbe80a25333dd02a552f57d978c78
ruess <ruess>
2002-04-03 01:04:18 +0000

    *** empty log message ***

---
+++ src/nonlin.ml
@@ -0,0 +1,239 @@
+
+(*i
+ * The contents of this file are subject to the ICS(TM) Community Research
+ * License Version 1.0 (the ``License''); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.icansolve.com/license.html.  Software distributed under the
+ * License is distributed on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing rights and limitations under the License.  The Licensed Software
+ * is Copyright (c) SRI International 2001, 2002.  All rights reserved.
+ * ``ICS'' is a trademark of SRI International, a California nonprofit public
+ * benefit corporation.
+ * 
+ * Author: Harald Ruess
+ i*)
+
+(*i*)
+open Tools
+open Hashcons
+open Mpa
+open Term
+open Sym
+(*i*)
+
+
+(*s Theory-specific recognizers *)
+
+let is_interp a =
+  Sym.is_nonlin (Term.sym_of a)
+
+
+(*s Apply [f] at uninterpreted positions. *)
+
+let rec iter f a =          
+  match a.node with
+    | App({node=Interp(Nonlin(op))},l) ->
+	(match op, l with
+	   | Expt(n), [x] -> f x
+	   | Mult, _::_::_ -> List.iter (iter f) l
+	   | _ -> assert false)
+    | _ ->
+	f a
+
+(*s [occurs a b] iff [a] occurs interpreted in [b].. *)
+
+let rec occurs a b =    
+  (a === b) ||
+  (match b.node with
+     | App({node=Interp(Nonlin(op))},l) ->
+	 (match op, l with
+	    | Expt _, [x] -> occurs a x
+	    | Mult, _ -> List.exists (occurs a) l
+	    | _ -> assert false)
+     | _ ->
+	 false)
+
+
+
+(*s Destructors. *)
+
+let d_mult a =
+  match a.node with
+    | App({node=Interp(Nonlin(Mult))}, l) -> Some(l)
+    | _ -> None
+
+let d_expt a =
+  match a.node with
+    | App({node=Interp(Nonlin(Expt(n)))},[x]) -> Some(n,x)
+    | _ -> None
+
+(* [normal_of a] returns a normalized representation of a nonlinear
+ multiplication; e.g. [x^3 * y * z^2] is represented as [(3,x),(1,y),(2,z)] *)
+
+let expt_of a =
+  match d_expt a with
+    | Some(n,x) -> (n,x)
+    | None -> (1,a)
+
+
+(*s Constructors. *)
+
+let rec mk_mult (a,b) =
+  if a === b then mk_expt 2 a else
+  match d_expt a, d_expt b with
+    | Some(n,x), Some(m,y) when x === y ->
+	mk_expt (n + m) x
+    | Some(n,x), _ when x === b ->
+	mk_expt (n + 1) x
+    | _, Some(m,y) when y === a ->
+	mk_expt (m + 1) y
+    | _ ->
+	(match Linarith.d_interp a with
+	   | Some(f,xl) ->
+	       mk_linmult f xl b
+	   | None ->
+	       (match Linarith.d_interp b with
+		  | Some(g,yl) ->
+		      mk_linmult g yl a
+		  | None ->
+		      (match d_mult a, d_mult b with
+			 | Some(xl), Some(yl) ->
+			     of_list (multl xl yl)
+			 | Some(xl), None ->
+			     of_list (multl xl [b])
+			 | None, Some(yl) ->
+			     of_list (multl yl [a])
+			 | _ ->
+			     let (a,b) = Term.order a b in
+			     Term.make (Sym.mk_mult, [a;b]))))
+
+and mk_multl al = 
+  match al with
+    | [] -> Linarith.mk_num Q.one
+    | [x] -> x
+    | x :: xl -> mk_mult (x, mk_multl xl)
+
+and mk_linmult f al b =
+  match f, al with
+    | Sym.Num(q),[] ->
+	Linarith.mk_multq q b
+    | Sym.Multq(q), [x] ->
+	Linarith.mk_multq q (mk_mult (x,b))
+    | Sym.Add, xl ->
+	let yl = List.map (fun x -> mk_mult (b,x)) xl in
+	Linarith.mk_addl yl
+    | _ ->
+	failwith "Ill-formed expression"
+
+and multl l1 l2 =             
+  match l1, l2 with
+    | [], _ -> l2
+    | _ , [] -> l1
+    | m1 :: l1', m2 :: l2' ->
+	let n1,x1 = expt_of m1 in
+	let n2,x2 = expt_of m2 in
+	let cmp = Term.cmp x1 x2 in
+	if cmp = 0 then
+	  let n = n1 + n2 in
+	  if n = 0 then 
+	    multl l1' l2'
+	  else 
+	    (mk_expt n x1) :: (multl l1' l2')
+	else if cmp < 0 then
+	  m1 :: multl l1' l2
+	else (* cmp > 0 *)
+	  m2 :: multl l1 l2'
+
+and of_list l =
+  match l with
+    | [] -> Linarith.mk_one
+    | [x] -> x
+    | _ -> Term.make (Sym.mk_mult, l)
+
+and mk_expt n a =
+  if n = 0 then
+    Linarith.mk_one
+  else if n = 1 then
+    a
+  else 
+    (match Linarith.d_interp a with
+       | Some(f,xl) ->
+	   (match f, xl with
+	     | Sym.Num(q),[] ->
+		 Linarith.mk_num (Mpa.Q.expt q n)
+	     | Sym.Multq(q), [x] ->
+		 Linarith.mk_multq (Mpa.Q.expt q n) (mk_expt n x)
+	     | Sym.Add, [x;y] ->
+		 mk_mult (mk_expt (n - 2) a,
+			  Linarith.mk_addl [mk_expt 2 x;
+					    Linarith.mk_multq (Mpa.Q.of_int 2) (mk_mult (x,y));
+					    mk_expt 2 y])		
+	     | _ ->
+		 Term.make (Sym.mk_expt(n), [a]))
+       | None ->
+	   (match d_expt a with
+	      | Some(m, x) when x === a ->
+		  mk_expt (n + m) a
+	      | _ ->
+		  (match d_mult a with
+		     | Some(xl) ->
+			 of_list (List.map (mk_expt n) xl)
+		     | None ->
+			 Term.make (Sym.mk_expt(n), [a]))))
+
+
+(*s Apply term transformer [f] at uninterpreted positions. *)
+
+let rec norm f =
+  let rec loop a =
+    match Term.destruct a with
+      | {node=Interp(Nonlin(op))},l ->
+	  (match op, l with
+	     | Mult, [x;y] -> mk_mult (loop x, loop y)
+	     | Mult, _::_::_ -> mk_multl (Term.mapl loop l)
+	     | Expt(n), [x] -> mk_expt n (loop x)
+	     | _ -> failwith "Nonlin.norm: ill-formed arithmetic expression")
+      | _ ->
+	  f a
+  in
+  loop
+
+
+(*s Interface for sigmatizing arithmetic terms. *)
+
+let rec sigma op l =
+  match op, l with
+    | Mult, [x; y] -> mk_mult (x,y)
+    | Mult, _::_::_ -> mk_multl l
+    | Expt(n), [x] -> mk_expt n x
+    | _ -> failwith("Nonlin.sigma: ill-formed argument")
+
+
+(*s Solving of nonlinear equations. *)
+
+let solve rename (a,b) =
+  match is_interp a, is_interp b with
+    | true, true ->                    (* case: [x^2*y = x*y^2] *)
+	let x = rename Type.mk_top in
+	let y = rename Type.mk_top in
+	[(order x y); (x,a); (y,b)]
+    | false, true ->                   (* case: [x^2*y = z] *)      
+        if occurs a b then
+	  let x = rename Type.mk_top in
+	  [(order x a); (x, b)]
+	else 
+	  [a,b]
+    | true, false ->                          (* case: [x^2*y = z] *)
+	if occurs b a then
+	  let x = rename Type.mk_top in
+	  [(order x b); (x, a)]
+	else
+	  [b,a]
+    | false, false ->
+	[order a b]
+
+
+
+
+
---
+++ src/nonlin.mli
@@ -0,0 +1,63 @@
+
+(*i
+ * ICS - Integrated Canonizer and Solver
+ * Copyright (C) 2001-2004 SRI International
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the ICS license as published at www.icansolve.com
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * ICS License for more details.
+ * 
+ * Author: Harald Ruess
+ i*)
+
+(*i*)
+open Mpa
+open Term
+(*i*)
+
+(*s Module [Nonlin]: Constructors, recognizers, and accessors for
+ nonlinear arithmetic terms. *)
+
+(*s [is_interp a] holds iff [a] is a nonlinear arithmetic terms; that is,
+  [a] is a nonlinear multiplication ([mult]) *)
+
+val is_interp: Term.t -> bool
+
+(* All non-arithmetic terms, that is, terms [a] for which [is_linarith a]
+   fails are considered to be uninterpreted. [iter f a] is used to apply
+   procedure [f] at uninterpreted positions of [a]. *)
+
+val iter: (Term.t -> unit) -> Term.t -> unit        
+
+
+(*s Constructors for building up nonlinear arithmetic terms. *)
+
+val mk_mult : Term.t * Term.t -> Term.t
+val mk_expt : int -> Term.t -> Term.t
+
+(*s Destructors. *)
+
+val d_mult : Term.t -> Term.t list option
+val d_expt : Term.t -> (int * Term.t) option
+
+(*s sigmatize *)
+
+val sigma : Sym.nonlin -> Term.t list -> Term.t
+
+(*s Given a substitution [s] and a term [a], [norm s a] replaces uninterpreted
+  occurrences [x] in [a] with term [y], if there is a binding [x |-> y] in [s].
+  The resulting term is in polynomial normal form.  Thus, [norm] can be thought
+  of as the the composition of applying substitution [s] to [a] followed by 
+  sigmatizing each arithmetic subterm by the function [sigma] above. *)
+
+val norm: (Term.t -> Term.t) -> Term.t -> Term.t
+
+(*s Partial solver. *)
+
+val solve : rename:(Type.t -> Term.t)
+               -> Term.t * Term.t -> (Term.t * Term.t) list
+
