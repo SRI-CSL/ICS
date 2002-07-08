5dcb1e569999c0bfb7850f2ffc5290f1183b9acf
ruess <ruess>
2002-07-08 17:35:08 +0000

    *** empty log message ***

---
+++ src/boolean.mli
@@ -0,0 +1,30 @@
+
+(*i
+ * The contents of this file are subject to the ICS(TM) Community Research
+ * License Version 1.0 (the ``License''); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.icansolve.com/license.html.  Software distributed under the
+ * License is distributed on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing rights and limitations under the License.  The Licensed Software
+ * is Copyright (c) SRI International 2001, 2002.  All rights reserved.
+ * ``ICS'' is a trademark of SRI International, a California nonprofit public
+ * benefit corporation.
+ * 
+ * Author: Harald Ruess
+ i*)
+
+(*s Module [Boolean]: Manipulating Boolean constants 
+ [mk_true] and [mk_false]. *)
+
+val is_interp : Term.t -> bool
+
+val mk_true : Term.t
+val mk_false : Term.t
+
+val is_true : Term.t -> bool
+val is_false : Term.t -> bool
+
+val sigma : Sym.boolean -> Term.t list -> Term.t
+
+val solve : Term.t * Term.t -> (Term.t * Term.t) list
