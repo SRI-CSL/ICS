5cb1d925253228eda971099b9e9d74f5078d2267
ruess <ruess>
2002-07-14 21:44:11 +0000

    added is_slack

--- src/a.mli
+++ src/a.mli
@@ -23,11 +23,13 @@ type t
 
 (*s Accessors. [solution s] returns a list of equalities in the form [(x,a)],
  where [x] is a variable and [a] is an arithmetic term fulfilling [Arith.is_arith].
- [cnstrnt s] returns a list of constraints [(x,c)], where [x] is a variable and
+ [domains s] returns a list of constraints [(x,c)], where [x] is a variable and
  [c] is a constraints, with the meaning that [x] is interpreted over the domain
  corresponding to [c]. *)
 
-val solution_of : t -> (Term.t * Term.t) list
+val solutions : t -> (Term.t * Term.t) list
+
+val domains : t -> (Term.t * Cnstrnt.t) list
 
 (*s Pretty-printing. *)
 
@@ -51,12 +53,15 @@ val inv : t -> Term.t -> Term.t
 val use : t -> Term.t -> Term.Set.t
 
 
+(*s List all constraints with finite extension. *)
+
+val split : t -> (Term.t * Cnstrnt.t) list
+
 (*s [extend s (a,b)] installs a new binding [a |-> b] into [s]. 
   It assumes that [a] is not yet in the domain of [s]. 
   Also, [a],[b] must be valid lhs and rhs, respectively. *)
 
 val extend : Term.t -> t -> Term.t * t
-
 
 (*s [merge (a,b) s] installs an equality [a = b] into [s]. *)
 
@@ -69,4 +74,8 @@ val add : (Term.t * Cnstrnt.t) -> t -> (t * Veqs.t)
 
 (*s Constraint. *)
 
-val to_cnstrnt : t -> Term.t -> Cnstrnt.t option
+val cnstrnt : t -> Term.t -> Cnstrnt.t
+
+(*s Test for internal parameters. *)
+
+val is_slack : Term.t -> bool
