55569cb77e4d8a45665820efff93e382b22608c9
ruess <ruess>
2002-10-01 23:22:08 +0000

    added some docu

--- src/solution.mli
+++ src/solution.mli
@@ -18,9 +18,23 @@
  and manipulating conjunctions of equations [x = a], where
  [x] is a variable and [a] is a non-variable term. As an invariant,
  solution sets [s] are kept in functional form, that is, if [x = a]
- and [x = b] in [s], then [a] is identical with [b]. *)
+ and [x = b] in [s], then [a] is identical with [b]. In addition,
+ solution sets are injective, that is, [x = a] and [y = a] are not
+ in a solution set for [x <> y]. *)
 
 type t
+
+
+(*s [to_list s] returns a list of pairs [(x, a)], where [x] is a
+ variable and [a] a term for the equalities [x = a] in the 
+ solution set [s]. *)
+
+val to_list : t -> (Term.t * Term.t) list
+
+
+(*s Pretty-printing of solution sets. *)
+
+val pp : t Pretty.printer
 
 
 (*s [fold f s e] applies [f x a] to all [x = a] in
@@ -30,25 +44,25 @@ type t
 val fold : (Term.t -> Term.t -> 'a -> 'a) -> t -> 'a -> 'a
 
 
-(*s [apply s x] returns [b] if [x = b] is in [s], and raises [Not_found]
- otherwise. For non-variable terms [b], [apply s b] always returns [b]. *)
+(*s [apply s x] returns [b] if [x = b] is in [s], and 
+ raises [Not_found] otherwise. *)
 
 val apply : t -> Term.t -> Term.t
 
 
 (* [find s x] returns [b] if [x = b] is in [s], and [x] otherwise. 
- For non-variable [x], [find s x] always returns [x]. *)
+ For non-variable argument [a], [find s a] always returns [a]. *)
 
 val find : t -> Term.t -> Term.t
 
 
-(*s [inv s b] returns [x] if [x = b] is in [s]; otherwise [Not_found]
- is raised. *)
+(*s [inv s b] returns [x] if [x = b] is in [s]; 
+ otherwise [Not_found] is raised. *)
 
 val inv : t -> Term.t -> Term.t
 
 
-(*s [mem s x] holds iff [x = b] in [s]. *)
+(*s [mem s x] holds iff [x = b] is in [s]. *)
 
 val mem : t -> Term.t -> bool
 
@@ -58,16 +72,14 @@ val mem : t -> Term.t -> bool
 
 val occurs : t -> Term.t -> bool
 
+
 (*s [use s x] returns all [y] such that [y = a] in [s]
- and [x] is a variable in [vars a] (see module [Term]).
- However, if [s] has been manipulated using [adduse x ys s],
- then [ys] is returned. *)
+ and [x] is a variable in [Term.vars a]. *)
 
 val use : t -> Term.t -> Term.Set.t
 
 
-
-(*s The [empty] solution set *)
+(*s The [empty] solution set, which does not contain any equality. *)
 
 val empty : t
 
@@ -76,9 +88,12 @@ val empty : t
 
 val is_empty : t -> bool
 
-(*s Are the two solutions states identical. *)
 
-val unchanged : t -> t -> bool
+(*s [eq s t] tests if the solution sets [s] and [t] are identical
+ in the sense that the sets of equalities are stored at the same
+ memory location. *)
+
+val eq : t -> t -> bool
 
 
 (*s [restrict s x] removes equalities [x = a] from [s]. *)
@@ -86,41 +101,41 @@ val unchanged : t -> t -> bool
 val restrict : Term.t -> t -> t 
 
 
-(*s [extend a s] generates a fresh variable [x] and add [x = a]
- to [s]. It is assumed that there is no [y = a] already in [s]. 
- The result is the generated variable [x] together with the extended
- solution set. *)
-
-val extend : Term.t -> t -> Term.t * t
-
-(*s [union (x, b) s] *)
+(*s [union (x, b) s] adds an equality [x = b] to [s], 
+ possibly removing an equality [x = b'] in [s]. *)
 
 val union : Term.t * Term.t -> t -> t
 
-(*s [name e a] returns a variable [x] if there is
- a solution [x = a]. Otherwise, it creates a new name
- [x'] and installs a solution [x' = a] in [e]. *)
+
+(*s [name s a] returns the variable [x] if there is
+ an equation [x = a] in [s].  Otherwise, it creates a 
+ fresh variiable [x'] and installs a solution [x' = a] in [s]. *)
 
 val name : Term.t * t -> Term.t * t
 
 
-(*s [fuse norm v s sl] propagates the equalities in [sl] on the
-  right-hand side of equalities in [s]. 
-  The return value [(v', s', ch')] consists of an extension of
-  [v] by means of newly generated variable equalities, the
-  modified solution set [s'], and  the set of variables with a 
-  new canonical form in [v'] are returned. Thus,
-      [s' = { x = b' | x = b in s, b' = norm sl b, b' a non-variable}],
-      [v' = { x = y | x = b in s, y = norm sl b, y a variable}], and
-      [ch' = {x | x = y in v'}].  Here, it is assumed that [y] is
-  'more canonical' than [x] using the variable ordering specified in
-  the module [V]. *)
+(*s [fuse norm (p, s) r] propagates the equalities in [r] on 
+  the right-hand side of equalities in [s]. The return value [(p', s')] consists 
+  of an extension of the partition [p] with newly generated variable equalities
+  and a modified solution set [s'], which is obtained by transforming every
+  [x = b] in [s] to [x = norm(r)(b)].  Here, [norm(r)(b)] replaces occurrences
+  of [z] in [b] with [a] if [z = a] is in [r] and normalizes the result according
+  to the [norm] argument function. If [norm(r)(b)] reduces
+  to a variable, say [y], then the variable equality [x = y] is added to
+  the partition [p]. This may trigger an exception [Not_found], if the
+  disequality [x <> y] can be deduced from the partition [p].  This equality
+  is also propagated in the resulting [s'] in that every occurrence of [x]
+  is replaced by [y].  In case [norm(r)(b)] results in a non-variable [b'],
+  the equality [x = b'] is added if there is no [y = b'] already in the solution 
+  set. If there is such a [y], then the equality [x = y] is added to the 
+  partitioning [p] and only one of [x = b'], [y = b'] is retained in the
+  resulting solution set. *)
 
 val fuse : ((Term.t -> Term.t) -> Term.t -> Term.t)
               -> Partition.t * t -> (Term.t * Term.t) list -> Partition.t * t
 
 
-(*s [compose norm v s sl] is a [fuse] step followed by
+(*s [compose norm (p,s) r] is a [fuse] step followed by
  extending (and possibly overwriting [x = ...]) 
  the resulting [s'] with all [x = b], for [b]
  a non-variable term, in [sl]. If [b] is a variable, then
@@ -129,21 +144,13 @@ val fuse : ((Term.t -> Term.t) -> Term.t -> Term.t)
 val compose :  ((Term.t -> Term.t) -> Term.t -> Term.t)
                   -> Partition.t * t -> (Term.t * Term.t) list -> Partition.t * t
 
-(*s Normalize. *)
 
-val normalize : ((Term.t -> Term.t) -> Term.t -> Term.t) 
-                  -> Partition.t * t -> Partition.t * t
-
-
-(*s Pretty-printing. *)
-
-val pp : t Pretty.printer
-
-(*s Instantiating lhs with canonical variables. *)
-
-val inst : V.t -> t -> t
-
-(*s Change sets. *)
+(*s Every modification or addition of an equality [x = a] to
+ a solution set [s]--- using [union], [fuse], or [compose]---has the side-effect 
+ of adding [x] to the set of changed variables in [s]. This
+ set can be obtained by [changed s]. [reset s] resets the set of
+ changed variables in [s] to the empty set. *)
+ 
 
 val changed : t -> Term.Set.t
 
