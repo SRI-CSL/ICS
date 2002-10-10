c9f0b38f1e35fafa575f0b27d935153873f7cb68
ruess <ruess>
2002-10-10 11:51:51 +0000

    actually started with some docu...

--- src/tuple.mli
+++ src/tuple.mli
@@ -14,45 +14,41 @@
  * Author: Harald Ruess
  i*)
 
-(*s The module [Tuple] implements constructors for tuples and 
-  projections, normalization of tuple terms, a solver for the 
-  theory of tuples, and an abstract datatype for logical
-  contexts of equalities over tuples. *)
+(*s Module [Tuple]: canonizer and solver for the theory of tuples.
 
-(*s [is_tuple a] holds iff [a] is a projection or a tuple term.
-  Terms for which [is_tuple] is false are considered to be uninterpreted. *)
+  The signature of this theory consists of the nary function symbol
+  [Product] for constructing tuples and of the family of unary 
+  function symbols [Proj(i, n)], for integers [0 <= i < n], for projecting 
+  the $i$-th component (starting with $0$ and addressing in increasing 
+  order from left to right).
+
+  The theory of tuples is given as the initial algebra generated
+  by the axioms
+      \begin{array}{lcl}
+      ... & = & ...
+      \end{array}
+*)
+
+(*s [is_interp a] holds iff [a] is a projection of the form
+ [Proj(i, n)(x)] or a tuple term [Product(xl)].  Terms for 
+ which [is_interp] is false are considered to be uninterpreted. *)
 
 val is_interp : Term.t -> bool
 
-val d_interp : Term.t -> (Sym.tuple * Term.t list) option
-
-(*s [iter f a] applies [f] to all top-level uninterpreted subterms of [a]. *)
-
-val iter: (Term.t -> unit) -> Term.t -> unit 
-
-
-(*s [fold f a e] applies [f] at uninterpreted positions of [a] and 
- accumulates the results starting with [e]. *)
-
-val fold : (Term.t -> 'a -> 'a) -> Term.t -> 'a -> 'a
-
-(*s [map f a] applies [f] to all top-level uninterpreted subterms of [a],
- and rebuilds the interpreted parts in order. *)
-
-val map: (Term.t -> Term.t) -> Term.t -> Term.t
-
 
 (*s If the argument list [l] is of length [1], then
-  this term is returned. Otherwise, [tuple l] constructs the 
-  corresponding tuple term. *)
+  this term is returned. Otherwise, [tuple l] constructs
+  the corresponding tuple term. *)
 
 val mk_tuple : Term.t list -> Term.t
    
+
 (*s [proj i n a] is the constructor for the family of [i]-th projections
   from [n]-tuples, where [i] is any integer value between [0] and [n-1].
-  This constructor simplifies [proj i (tuple \list{a_0;...;a_n-1})] to [a_i]. *)
+  This constructor simplifies [proj i n (tuple \list{a_0;...;a_n-1})] to [a_i]. *)
  
 val mk_proj : int -> int -> Term.t -> Term.t
+
 
 (*s [sigma op l] applies the function symbol [op] from the tuple theory to
   the list [l] of terms. For the function symbol [Proj(i,n)] and the list [a],
@@ -62,21 +58,27 @@ val mk_proj : int -> int -> Term.t -> Term.t
 val sigma : Sym.tuple -> Term.t list -> Term.t
 
 
+(*s [fold f a e] applies [f] at uninterpreted positions of [a] and 
+ accumulates the results starting with [e]. *)
+
+val fold : (Term.t -> 'a -> 'a) -> Term.t -> 'a -> 'a
+
+
+(*s [map f a] applies [f] to all top-level uninterpreted 
+ subterms of [a], and rebuilds the interpreted parts in order. *)
+
+val map: (Term.t -> Term.t) -> Term.t -> Term.t
+
+
 (*s [solve (a,b)] returns a solved form for the equation [a = b]. 
   If this equation is inconsistent, then the exception [Exc.Inconsistent]
   is raised.  Otherwise, the solved form [(x1,e1),...,(xn,en)] is returned,
-  where [xi] are uninterpreted in the tuple theory and the [ei] are sigmatized.
-  The [ei] may also contain fresh variables.  The solver for equations over tuples 
-  and projections works by repetitively applying
-      \begin{tabular}{lcl}
-      [(tuple \list{$a_0$,\ldots,$a_n$},~tuple \list{$a_0$,\ldots,$a_n$})]
-          & = & [\list{($a_0$,$b_0$),\ldots,($a_n$,$b_n$)}] \\
-      [(proj i n a, b)] & = & [(a, tuple \list{$c_0$,\ldots,c_{i-1},b,...c_{n-1}})]
-      \end{tabular}
-      where [$c_i$] are fresh, and [b] at [i]-th position. *)
+  where [xi] are uninterpreted in the tuple theory and the [ei] are canonized.
+  The [ei] may also contain fresh variables. *)
 
 val solve : Term.t * Term.t -> (Term.t * Term.t) list
- 
-(*s Test if variables has been introduced by tuple solver. *)
+
+
+(*s Test if argument variable has been introduced by tuple solver. *)
 
 val is_fresh : Term.t -> bool
