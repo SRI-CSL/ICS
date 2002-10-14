811e4035618a490410553a40f3c21832b1f07945
ruess <ruess>
2002-10-14 06:26:26 +0000

    moved trace messages

--- src/v.ml
+++ src/v.ml
@@ -14,24 +14,31 @@
  * Author: Harald Ruess, N. Shankar
 i*)
 
+(*i*)
+open Term
+(*i*)
+
 type t = {
-  find : Term.t Term.Map.t;
-  inv : Term.Set.t Term.Map.t;
-  changed : Term.Set.t;
-  removable : Term.Set.t
+  find : Term.t Map.t;
+  inv : Set.t Map.t;
+  changed : Set.t;
+  removable : Set.t
 }
 
 let eq s t =
   s.find == t.find
 
-let apply s x = Term.Map.find x s.find
+let apply s x = 
+  match x with
+    | App _ -> raise Not_found 
+    | _ -> Map.find x s.find
 
 (*s Canonical representative of equivalence class containing [x] *)
 
 
 let rec find s x =
   try 
-    let y = Term.Map.find x s.find in
+    let y = apply s x in
     if Term.eq x y then y else find s y
   with 
       Not_found -> x
@@ -39,21 +46,23 @@ let rec find s x =
 (*s Inverse Map. *)
 
 let inv s x = 
-  Term.Map.find x s.inv
+  match x with
+    | App _ -> raise Not_found 
+    | _ -> Map.find x s.inv
 
 
 (*s Union of equivalence classes. *)
 
 let union x y s = 
-  Trace.msg "v" "Union" x Term.pp;
-  let find' = Term.Map.add x y s.find in
-  let invy = Term.Set.add x (try Term.Map.find y s.inv with Not_found -> Term.Set.empty) in
-  let inv' = Term.Map.add y invy s.inv in
+  Trace.msg "v" "Union" (x, y) Term.pp_equal;
+  let find' = Map.add x y s.find in
+  let invy = Set.add x (try Map.find y s.inv with Not_found -> Set.empty) in
+  let inv' = Map.add y invy s.inv in
   let changed' = 
-    Term.Set.add x s.changed in
+    Set.add x s.changed in
   let removable' = 
-    if Term.is_fresh_var x then
-      Term.Set.add x s.removable
+    if is_fresh_var x then
+      Set.add x s.removable
     else 
       s.removable
   in
@@ -64,23 +73,23 @@ let union x y s =
 
 
 let restrict x s =
-  Trace.msg "v" "Restrict" x Term.pp;
   try
     let y = apply s x in
-    {find = Term.Map.remove x s.find;
-     inv = 
-       (let inv' = Term.Map.remove x s.inv in
-        try 
-	  let invy = inv s y in
-	  let invy' = Term.Set.remove x invy in
-	  if Term.Set.is_empty invy' then
-	    Term.Map.remove y inv'
-	  else 
-	    Term.Map.add y invy' inv'
-	with 
-	    Not_found -> inv');
-     changed = Term.Set.remove x s.changed;
-     removable = Term.Set.remove x s.removable}
+      Trace.msg "v" "Restrict" x pp;
+      {find = Map.remove x s.find;
+       inv = 
+	 (let inv' = Map.remove x s.inv in
+            try 
+	      let invy = inv s y in
+	      let invy' = Set.remove x invy in
+		if Set.is_empty invy' then
+		  Map.remove y inv'
+		else 
+		  Map.add y invy' inv'
+	    with 
+		Not_found -> inv');
+       changed = Set.remove x s.changed;
+       removable = Set.remove x s.removable}
   with
       Not_found -> s
 
@@ -90,7 +99,7 @@ let restrict x s =
 let find' s x =
   let rec loop acc x =
     try
-      let y = Term.Map.find x s.find in
+      let y = Map.find x s.find in
       if Term.eq x y then
 	(acc, y)
       else 
@@ -112,21 +121,21 @@ let is_equal s x y =
 (*s The empty context. *)
 
 let empty = {
-  find = Term.Map.empty;
-  inv = Term.Map.empty;
-  changed = Term.Set.empty;
-  removable = Term.Set.empty
+  find = Map.empty;
+  inv = Map.empty;
+  changed = Set.empty;
+  removable = Set.empty
 }
 
 let is_empty s = 
-  (s.find == Term.Map.empty)
+  (s.find == Map.empty)
 
 (*s Return the set of removable variables. *)
 
 let removable s = s.removable
 
 let remove s =
-  Term.Set.fold restrict (removable s) s
+  Set.fold restrict (removable s) s
 
 
 (*s Starting from the canonical representative [x' = find s x], the
@@ -137,7 +146,7 @@ let fold s f x =
   let rec loop y acc =
     let acc' = f y acc in
     try
-      Term.Set.fold loop (Term.Map.find y s.inv) acc'
+      Set.fold loop (Map.find y s.inv) acc'
     with
 	Not_found -> acc'
   in
@@ -158,7 +167,7 @@ let merge e s =
 
 (*s Extension of the equivalence class for [x]. *)
 
-let ext s x = fold s Term.Set.add x Term.Set.empty
+let ext s x = fold s Set.add x Set.empty
 
 
 (*s Iteration. *)
@@ -167,7 +176,7 @@ let iter s f x =
   let rec loop y =
     f y;
     try
-      Term.Set.iter loop (Term.Map.find y s.inv)
+      Set.iter loop (Map.find y s.inv)
     with
 	Not_found -> ()
   in
@@ -180,7 +189,7 @@ let exists s p x =
   let rec loop y =
     p y || 
     try
-      Term.Set.exists loop (Term.Map.find y s.inv)
+      Set.exists loop (Map.find y s.inv)
     with
 	Not_found -> false
   in
@@ -191,7 +200,7 @@ let for_all s p x =
   let rec loop y =
     p y &&
     try
-      Term.Set.for_all loop (Term.Map.find y s.inv)
+      Set.for_all loop (Map.find y s.inv)
     with
 	Not_found -> true
   in
@@ -200,11 +209,14 @@ let for_all s p x =
 (*s Changed. *)
 
 let is_changed s x = 
-  Term.Set.mem x s.changed
+  Set.mem x s.changed
 
 let changed s = s.changed
 
-let reset s = {s with changed = Term.Set.empty}
+let reset s = 
+  if Set.is_empty(s.changed) then s else
+    {s with changed = Set.empty}
+
 
 (*s Choose an element satisfying some property. *)
 
@@ -231,14 +243,14 @@ let choose s p x =
  themselves in the domain of [find]. *)
 
 let canrepr s = 
-  Term.Map.fold 
+  Map.fold 
     (fun _ y acc -> 
-       if Term.Map.mem y s.find then
+       if Map.mem y s.find then
 	 acc
        else 
-	 Term.Set.add y acc)
+	 Set.add y acc)
     s.find
-    Term.Set.empty
+    Set.empty
 
 
 (*s Representation of the equivalence classes as a map with the
@@ -246,11 +258,11 @@ let canrepr s =
  in the codomain. The following is not terribly efficient. *)
 
 let partition s =
-  Term.Set.fold 
+  Set.fold 
     (fun x -> 
-       Term.Map.add x (ext s x)) 
+       Map.add x (ext s x)) 
     (canrepr s) 
-    Term.Map.empty
+    Map.empty
     
 
 (*s Pretty-printing. *)
@@ -258,6 +270,6 @@ let partition s =
 let pp fmt s =
   if not(is_empty s) then
     let m = partition s in
-    let l = Term.Map.fold (fun x ys acc -> (x, Term.Set.elements ys) :: acc) m [] in
+    let l = Map.fold (fun x ys acc -> (x, Set.elements ys) :: acc) m [] in
     Pretty.string fmt "\nv:";
     Pretty.map Term.pp (Pretty.set Term.pp) fmt l
