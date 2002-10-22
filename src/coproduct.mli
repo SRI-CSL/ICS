744c450648c9e27521041e5a27b6c332d08e9175
ruess <ruess>
2002-10-22 10:02:37 +0000

    coproducts

---
+++ src/coproduct.mli
@@ -0,0 +1,33 @@
+
+(*i
+ * The contents of this file are subject to the ICS(TM) Community Research
+ * License Version 1.0 (the ``License''); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.icansolve.com/license.html.  Software distributed under the
+ * License is distributed on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing rights and limitations under the License.  The Licensed Software
+ * is Copyright (c) SRI International 2001, 2002.  All rights reserved.
+ * ``ICS'' is a trademark of SRI International, a California nonprofit public
+ * benefit corporation.
+ * 
+ * Author: Harald Ruess
+ i*)
+
+(*s Module [Coproduct]: canonizer and solver for the theory of tuples. *)
+
+val mk_inl : Term.t -> Term.t
+val mk_inr : Term.t -> Term.t
+val mk_outl : Term.t -> Term.t
+val mk_outr : Term.t -> Term.t
+
+val mk_inj : int -> Term.t -> Term.t
+val mk_out : int -> Term.t -> Term.t
+ 
+val sigma : Sym.coproduct -> Term.t list -> Term.t
+
+val fold : (Term.t -> 'a -> 'a) -> Term.t -> 'a -> 'a
+
+val map: (Term.t -> Term.t) -> Term.t -> Term.t
+
+val solve : Fact.equal -> Fact.equal list
