f2132149245ca79bca02360088073c27b97b5e7d
owre <owre>
2002-10-24 22:02:18 +0000

    Added SIGINT to the list

--- src/ocaml-signals.c
+++ src/ocaml-signals.c
@@ -1,16 +1,18 @@
-
+#include <stdio.h>
 #include <signal.h>
 
 struct sigaction lisp_sigill_handler, ocaml_sigill_handler;
 struct sigaction lisp_sigstksz_handler, ocaml_sigstksz_handler;
 struct sigaction lisp_sigsegv_handler, ocaml_sigsegv_handler;
 struct sigaction lisp_sigtrap_handler, ocaml_sigtrap_handler;
+struct sigaction lisp_sigint_handler, ocaml_sigint_handler;
 
 void set_ocaml_handlers(void) {
   sigaction(SIGILL, &ocaml_sigill_handler, &lisp_sigill_handler);
   sigaction(SIGSTKSZ, &ocaml_sigstksz_handler, &lisp_sigstksz_handler);
   sigaction(SIGSEGV, &ocaml_sigsegv_handler, &lisp_sigsegv_handler);
   sigaction(SIGTRAP, &ocaml_sigtrap_handler, &lisp_sigtrap_handler);
+  sigaction(SIGINT, &ocaml_sigint_handler, &lisp_sigint_handler);
 }
 
 void restore_lisp_handlers(void) {
@@ -18,4 +20,5 @@ void restore_lisp_handlers(void) {
   sigaction(SIGSTKSZ, &lisp_sigstksz_handler, &ocaml_sigstksz_handler);
   sigaction(SIGSEGV, &lisp_sigsegv_handler, &ocaml_sigsegv_handler);
   sigaction(SIGTRAP, &lisp_sigtrap_handler, &ocaml_sigtrap_handler);
+  sigaction(SIGINT, &lisp_sigint_handler, &ocaml_sigint_handler);
 }
