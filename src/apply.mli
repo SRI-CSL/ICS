418289280692b130f950f125a63262de44faaa1d
ruess <ruess>
2002-11-07 04:36:47 +0000

    added comments

--- src/apply.mli
+++ src/apply.mli
@@ -1,5 +1,5 @@
 
-(*i
+(*
  * The contents of this file are subject to the ICS(TM) Community Research
  * License Version 1.0 (the ``License''); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
@@ -12,23 +12,58 @@
  * benefit corporation.
  * 
  * Author: Harald Ruess
- i*)
+ *)
 
-(*i*)
-open Mpa
-open Term
-(*i*)
+(** Module [Apply]: 
 
-(*s Module [Apply]: Theoruy of function application and abstraction *)
+  Theory of function application and abstraction. Terms in this theories are
+  build from the symbols [apply(r)] and [abs] for function application and
+  abstraction, respectively.  The [r] argument in [apply(r)] is used to infer
+  a constraint [c] for application terms if [r] is of the form [Some(c)]. 
+  deBruijn indices are used to in represent bound variables (see module {!Var}).
+  All other terms are considered to be uninterpreted.
 
-val mk_apply : (Sym.t -> Term.t list -> Term.t) 
-                  -> Cnstrnt.t option -> Term.t -> Term.t list 
-                    -> Term.t
+  Only strongly-normalizing terms are considered here, since, otherwise,
+  beta-reduction may lead to nontermination in building function applications.
+*)
+
+
+(** {Signature} *)
+
+val apply : Cnstrnt.t option -> Sym.t
+(** Family of function symbols for representing function application. *)
+
+val abs : Sym.t
+(** Function symbol for representing functional abstraction. *)
+
+
+(** {Constructors} *)
+
+
+val mk_apply : (Sym.t -> Term.t list -> Term.t)
+                   -> Cnstrnt.t option -> Term.t -> Term.t list -> Term.t
+(** [mk_apply sigma c a al] builds an application with range type [c] by
+ applying term [a] to a list of argument terms [al].  In addition, it performs
+ beta-reduction. The [sigma] argument is used to further simplify beta redeces. *)
 
 val mk_abs : Term.t -> Term.t
+(** [mk_abs a] lambda-abstracts [a]. All free occurrences in [a] of free variables 
+ of the form [!0] are bound by this abstraction. *)
+
+
+(** {Operators} *)
 
 val sigma : Sym.apply -> Term.t list -> Term.t
+(** Depending on the function symbol [f], [sigma f al] uses the constructors
+ {!mk_apply} and {!mk_abs} to compute  the normal-form of applying [f] to the 
+ arguments [al]. *)
 
 val tau : (Term.t -> Cnstrnt.t) -> Sym.apply -> Term.t list -> Cnstrnt.t
+(** [tau c f al] computes the constraint [i] if [f] is of the form [apply(Some(i))]
+ and [al] is a unary list. *)
 
 val map: (Term.t -> Term.t) -> Term.t -> Term.t
+(** [map f a] applies the term transformer [f] to each uninterpreted 
+  subterm of [a] and rebuilds the term [a] by using the simplifying constructors
+  [mk_apply] and [mk_abs]. *)
+  
--- src/can.mli
+++ src/can.mli
@@ -1,7 +1,4 @@
-
-
-
-(*i
+(*
  * The contents of this file are subject to the ICS(TM) Community Research
  * License Versicn 1.0 (the ``License''); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
@@ -14,20 +11,33 @@
  * benefit corporation.
  * 
  * Author: Harald Ruess
- i*)
+ *)
 
 
-(*s Module [Can]: Various canonizers and normalizers.*)
+(** Module [Can]: Term canonizers and normalization functions for atoms. *)
 
-(*s Canonization of terms. *)
 
 val term : Context.t -> Term.t -> Term.t
+(** [term s a] computes a canonical normal form of [a] with respect
+ to the equality theory in [s]. That is, [Term.eq (term s a) (term s b)]
+ holds if and only if the [term s a] and [term s b] are equal in the
+ equality theory of [s]. *)
 
 val arith : Context.t -> Sym.arith -> Term.t list -> Term.t
+(** [arith s op al] yields a canonical term [b] such 
+ that [Term.mk_app op al] is equal to [b] in the equality
+ theory of [s]. *)
 
 val eq : Context.t -> Term.t -> Term.t -> bool
-
-
-(*s Normalization of atoms using canonization. *)
+(** [eq s a b] is [true] if and only if [a] and [b] are
+ equal in the equality theory of [s]. *)
 
 val atom : Context.t -> Atom.t -> Atom.t
+(** [atom s a] normalizes atom [a] in context [s]. 
+ For equalities [e], this function is a canonizer in the
+ sense that [atom s e] reduces to [Atom.mk_true] if [e]
+ holds in [s] and [Atom.mk_false] if the negation of 
+ [e] holds in [s].  Denumerators in equalities and inequalities 
+ are cross-multiplied.  Similarly, constraints [c] of the form [a in i], 
+ [atom s c] are normalized by crossmultiplying denumerators with
+ a fixed sign in [s]. *)
