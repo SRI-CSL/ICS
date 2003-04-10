2ecc60c6be13630c1d4804404545d674ec89a325
ruess <ruess>
2003-04-10 17:04:30 +0000

    fix

--- src/arr.ml
+++ src/arr.ml
@@ -45,16 +45,16 @@ let mk_select =
 	| _ ->
 	    select [b; j]
 
-let mk_update = 
+let rec mk_update = 
   let update = mk_app (Arrays(Update)) in
     fun is_equal a j y ->
       match a with
-	| App(Arrays(Update), [b; i; x])
+	| App(Arrays(Update), [b; i; x]) ->
 	    (match is_equal i j with
 	       | Three.Yes -> 
 		   update [b; i; y]
 	       | Three.No when Term.cmp i j > 0 ->
-		   mk_update is_equal (mk_update is_equal b i x) j y
+		   mk_update is_equal (mk_update is_equal b j y) i x
 	       | _ -> 
 		   update [a; j; y])
 	| _ ->
