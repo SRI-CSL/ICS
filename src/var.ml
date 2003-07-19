d7218a13c3a87b7e862a6e8307e1a8baf296d6e5
ruess <ruess>
2003-07-19 06:55:03 +0000

    variable ordering needed refinement to ensure slacks are always smallest

--- src/var.ml
+++ src/var.ml
@@ -4,20 +4,20 @@ open Format
 
    
 type t =  
-  | Slack of int * Dom.t * slack
+  | Slack of int * slack
   | External of Name.t * Dom.t option
   | Rename of Name.t * int * Dom.t option
   | Fresh of Th.t * int * Dom.t option
   | Bound of int                       (* Bound variables for quantification *)
 
-and slack = Zero | Nonneg
+and slack = Zero | Nonneg of Dom.t
      
 and var = t
 
 (** {6 Constructors} *)
 
 let mk_external n d = External(n, d)
-let mk_slack k d m = Slack(k, d, m)
+let mk_slack k sl = Slack(k, sl)
 let mk_rename n k d = Rename(n, k, d)
 let mk_fresh th k d = Fresh(th, k, d)
 let mk_free k = Bound(k)
@@ -29,9 +29,9 @@ let name_of = function
   | External(n, _) -> n
   | Rename(n, i, _) ->  
       Name.of_string (Format.sprintf "%s!%d" (Name.to_string n) i)
-  | Slack(i, d, Zero) ->  
+  | Slack(i, Zero) ->  
       Name.of_string (Format.sprintf "%s!%d" "k0" i)
-  | Slack(i, d, Nonneg) ->  
+  | Slack(i, Nonneg(d)) ->  
       Name.of_string (Format.sprintf "%s!%d" "k" i)
   | Fresh(th, i, _) -> 
       Name.of_string (Format.sprintf "%s!%d" (Th.to_string th) i)
@@ -39,7 +39,8 @@ let name_of = function
       Name.of_string (Format.sprintf "!%d" n)
 
 let dom_of = function 
-  | Slack(_, d, _) -> d
+  | Slack(_, Zero) -> Dom.Int
+  | Slack(_, Nonneg(d)) -> d
   | External(_, Some(d)) -> d
   | Fresh(_, _, Some(d)) -> d
   | Rename(_, _, Some(d)) -> d
@@ -56,46 +57,45 @@ let is_real x =
 
 (** {6 Variable ordering} *)
 
-(* slack < external < fresh < renaming *)
+(* slack < external < fresh < renaming < bound *)
 let rec cmp x y =
-  let c1 = domcmp x y in
-    if c1 <> 0 then c1 else
-      (match x, y with       
-	 | Fresh _, Rename _ -> -1
-	 | Rename _, Fresh _ -> 1
-	 | External _, (Rename _ | Fresh _) -> -1 
-	 | (Rename _ | Fresh _), External _ -> 1
-	 | Slack _, (Rename _ | External _ | Fresh _) -> -1
-	 | (Rename _ | External _ | Fresh _), Slack _ -> 1  
-	 | Rename(n, i, _), Rename(m, j, _) -> 
-	     let c2 = Pervasives.compare i j in
-	       if c2 != 0 then c2 else Name.cmp n m
-	 | External(n, _), External(m, _) -> 
-	     Name.cmp n m
-	 | Slack(i, _, m), Slack(j, _, n) ->
-	     (match m, n with
-		| Zero, Nonneg -> -1
-		| Nonneg, Zero -> 1
-		| _ -> Pervasives.compare j i)
-	 | Fresh(n, i, _), Fresh(m, j, _) ->
-	     let c1 = -(Pervasives.compare i j) in
-	       if c1 != 0 then c1 else Pervasives.compare n m
-	 | _ -> 
-	     Pervasives.compare x y)
+  match x, y with
+    | Slack(i, Zero), Slack(j, Zero) ->
+	Pervasives.compare j i
+    | Slack(_, Zero), Slack(j, Nonneg _) -> -1
+    | Slack(_, Nonneg _), Slack(j, Zero) -> 1
+    | Slack(i, Nonneg(d)), Slack(j, Nonneg(e)) ->
+	let c1 = Dom.cmp d e in
+	  if c1 != 0 then c1 else
+	    Pervasives.compare j i
+    | Slack _, _ ->  -1
+    | _, Slack _ -> 1
+    | External(n, d), External(m, e) -> 
+	let c1 = domcmp d e in
+	  if c1 != 0 then c1 else Name.cmp n m
+    | External _, _ -> -1
+    | Fresh(n, i, d), Fresh(m, j, e) ->
+	let c1 = domcmp d e in
+	  if c1 != 0 then c1 else 
+	    let c2 = Pervasives.compare i j in
+	      if c2 != 0 then c2 else Pervasives.compare n m
+    | Fresh _, _ -> -1
+    | Rename(n, i, d), Rename(m, j, e) -> 
+	let c1 = domcmp d e in
+	  if c1 != 0 then c1 else 
+	    let c2 = Pervasives.compare i j in
+	      if c2 != 0 then c2 else Name.cmp n m
+    | Rename _, _ -> -1
+    | Bound(i), Bound(j) -> 
+	Pervasives.compare i j
+    | Bound _ , _ -> 1
 
-and domcmp x y =
-  let dom = function 
-    | Slack(_, d, _) -> Some(d)
-    | External(_, d) -> d
-    | Fresh(_, _, d) -> d
-    | Rename(_, _, d) -> d
-    | Bound _ -> None
-  in
-    match dom x, dom y with
-      | None, None -> 0
-      | Some _, None -> -1
-      | None, Some _ -> 1
-      | Some d, Some e -> Dom.cmp d e
+and domcmp d e =
+  match d, e with
+    | None, None -> 0
+    | Some _, None -> -1
+    | None, Some _ -> 1
+    | Some d, Some e -> Dom.cmp d e
 
 
 let (<<<) x y = (cmp x y <= 0)
@@ -106,7 +106,9 @@ let (<<<) x y = (cmp x y <= 0)
 let is_var = function External _ -> true | _ -> false
 let is_rename = function Rename _ -> true | _ -> false
 let is_free = function Bound _ -> true | _ -> false
-let is_slack m = function Slack(_, _, m') when m = m' -> true | _ -> false
+let is_slack sl = function Slack(_, sl') when sl = sl' -> true | _ -> false
+let is_nonneg_slack = function Slack(_, Nonneg _) -> true | _ -> false
+let is_zero_slack = function Slack(_, Zero) -> true | _ -> false
 let is_fresh th = function Fresh(th', _, _) when th = th' -> true | _ -> false
 
 let is_internal = function
@@ -157,6 +159,6 @@ let hash = function
   | External(n, _) -> (3 + Hashtbl.hash n) land 0x3FFFFFFF
   | Rename(n, i, _) -> (5 + i + Hashtbl.hash n) land 0x3FFFFFFF
   | Bound(i) -> (7 + i) land 0x3FFFFFFF
-  | Slack(n, d, Zero) -> (11 + Hashtbl.hash d) land 0x3FFFFFFF 
-  | Slack(n, d, Nonneg) -> (17 + Hashtbl.hash d) land 0x3FFFFFFF 
+  | Slack(n, Zero) -> (11 + Hashtbl.hash n) land 0x3FFFFFFF 
+  | Slack(n, Nonneg(d)) -> (17 + Hashtbl.hash d) land 0x3FFFFFFF 
   | Fresh(n, i, _) -> (21 + i + Hashtbl.hash n) land 0x3FFFFFFF 
