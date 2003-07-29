8f7e1ab75e1e649aefe0a12d92bce70f13764091
ruess <ruess>
2003-07-29 21:59:45 +0000

    *** empty log message ***

--- src/term.mli
+++ src/term.mli
@@ -1,4 +1,3 @@
-
 (*
  * The contents of this file are subject to the ICS(TM) Community Research
  * License Version 1.0 (the ``License''); you may not use this file except in
@@ -14,78 +13,128 @@
 
 (** Datatype of terms 
 
-  A term is either a variable [Var(s)] with a name [s] of type {!Name.t} or
-  an application [App(f,l)] of a function symbol of type {!Sym.t}
-  to a list of argument terms. 
-
   @author Harald Ruess
+
+  A {i term} is either a 
+  - {i variable} [x] ({!Var.t}) or an
+  - {i application} of a function symbol [f] ({!Sym.t}) to a list of 
+  argument terms [l].
+
+  Each term [a] has an integer value [hash a] associated with it.
 *)
 
 
 type t =
-  | Var of Var.t * int
-  | App of Sym.t * t list * int
+  | Var of Var.t * int               (* variables with indices *)
+  | App of Sym.t * t list * int      (* application with indices *)
+      (** Notice: all terms should be build using the constructors below.
+	This type definition is only visible for convenience to allow for
+	pattern matching on terms. *)
+      
+type trm = t  (* nickname *)
+
+val hash : t -> int
+  (** [hash a] returns a hash value such that {!Term.equal}[a b] implies 
+    [hash a = hash b].  Furthermore, [hash] restricted to variables is {i injective}. *)
+
+
+val pp : t Pretty.printer
+  (** Pretty-printing of terms. The exact outcome depends on the 
+    value {!Pretty.flag}. *)
+
+val to_string : t -> string
+  (** Pretty-printing a term to a string. [to_string a] is synonymous
+    with {!Pretty.to_string}[pp a]. *)
 
 
 (** {6 Variables} *)
  
 module Var : sig
-
-  (** {7 Constructors} *)
-    
+ 
   val mk_var : Name.t -> Dom.t option -> t
-
+    (** [mk_var n d] constructs an {i external} variable (see {!Var.mk_external})
+      of name [n] and optional domain constraint [d]. *)
+      
   val k : int ref
-    (** [k] is a global variable which is incremented by the {!Var.mk_rename} 
-      variable constructor below.  In addition, calls to {!Tools.do_at_reset} 
-      are resetting this variable to its default value [0]. *)
+    (** [k] is a global variable for generating names of internally generated
+      variables.  The value of this counter determines the notion of {i freshness}
+      as any internal variable with an index above [k] is assumed to be {i fresh}.
+      The counter [k] is incremented by the {!Var.mk_rename} variable constructors below.  
+      Calls to {!Tools.do_at_reset} are resetting this variable to its default value [0]. *)
     
   val mk_rename : Name.t -> int option -> Dom.t option -> t
     (** [mk_rename n None] constructs a fresh variable, where
       'fresh' means that the index part of this fresh variable
       (see Module {!Term.Var}) is larger than {!Term.Var.k}; as a side-effect,
       {!Term.Var.k} is incremented. [mk_fresh_var n Some(k)] simply constructs
-      a variable of name [n] and index [k]. *)
+      a variable of name [n] and index [k]; thus, the resulting variable
+      is not necessarily fresh. *)
 
   val mk_slack : int option -> Var.slack -> t
+    (** [mk_slack None sl] constructs a {i fresh} slack variable, 
+      and [mk_slack Some(i) sl] construct a slack variable of index [i].
+      If [sl] is [Var.Zero], then a {i zero slack} variable is created, and
+      if [sl] is of the form [Var.Nonneg(d)], then a {i nonnegative slack}
+      variable with domain restriction [d] is generated. See also {!Var.mk_slack}. *)
 
   val mk_fresh : Th.t -> int option -> Dom.t option -> t
+    (** [mk_fresh th None d] creates a theory-specific fresh variable with optional
+      domain restriction [d], and  [mk_fresh th Some(i) d] creates the theory-specific
+      fresh variable of index [i] with optional domain restriction [d]. See also {!Var.mk_fresh}. *)
 
   val mk_free : int -> t
+    (** [mk_free i] creates a deBruijn index for bound variables. *)
 
-
-  (** {7 Recognizers} *)
-
+  (** Variables are partitioned into {i renaming}, {i external}, {i fresh}, {i free},
+    and {i slack variables}, and the following recognizers can be used to test membership
+    of a variable into a partition. Thus, for every variable [x], exactly one of the tests
+    below succeed. *)
   val is_rename : t -> bool
-    (** [is_rename a] holds if [a] is a variable and if it is
-      of category 'fresh' (see module {!Var}). *)
-
   val is_external : t -> bool
-
   val is_fresh : Th.t -> t -> bool
-
   val is_free : t -> bool
-
   val is_slack : t -> bool
+
+  (** Slacks are subpartitioned into {i zero} and {i nonnegative} slacks. *)
   val is_zero_slack : t -> bool
   val is_nonneg_slack : t -> bool
-
-
+    
   val is_internal : t -> bool
+    (** [is_internal x] holds iff [x] is either a renaming, a fresh, or a slack variable. *)
+    
   val is_dom : Dom.t -> t -> bool
-
+    (** [is_dom d x] holds iff the domain [e] of [x] as obtained by {!Term.Var.dom_of}[x]
+      is a subdomain of [d] (see also {!Dom.sub}). *)
+    
   val is_int : t -> bool
+    (** [is_int x] holds if [x] is restricted to be interpreted over the integers.
+      It is defined as [is_dom Dom.Int]. *)
+    
   val is_real : t -> bool
-
-
-  (** {7 Accessors} *)
-
+    (** [is_real x] holds if [x] is restricted to be interpreted over the reals.
+      It is defined as [is_dom Dom.Real]. *)
+    
+  val compare : t -> t -> int
+    (** Total variable ordering. It is designed to be fast, and does not
+      obey {!Var.cmp}. If [x == y] then [cmp x y = 0]. Furthermore, if
+      [cmp x y < 0] then [cmp y x > 0].  [Invalid_argument] is raised if one of 
+      the arguments is a non-variable. *)
+    
+  module Set : (Set.S with type elt = trm)
+    (** Sets of variables. Exception [Invalid_argument] is raised if a 
+      nonvariable term is added to any set. *)
+    
+  module Map : (Map.S with type key = trm)
+    (** Maplets with variables in the domain. [Invalid_argument] is raised
+      if a binding with a nonvariable term is added. *)
+    
   val name_of : t -> Name.t
     (** [name_of a] returns the name [n] if [a] is a variable of the
       form [Var(n)]; otherwise the result is undefined. *)
-
+    
   val dom_of : t -> Dom.t
-
+    (** [dom_of x] returns the domain restriction associated with [x] (see {!Var.dom_of}).
+      If there is no such restriction, [Not_found] is raised. *)
 
 end 
 
@@ -93,18 +142,13 @@ end
 
 module App : sig
 
-  (** {7 Constructors} *)
-
   val mk_const : Sym.t -> t
-    (** [mk_const c] constructs a function application 
-      of symbol [c] to the empty argument list. *)
+    (** [mk_const c] constructs a function application of symbol [c] to the empty
+      argument list. *)
 
   val mk_app : Sym.t -> t list -> t
-    (* [mk_app f l] constructs a function application of symbol [f] to a list [l]. *)
-
+    (** [mk_app f l] constructs a function application of symbol [f] to a list [l]. *)
   
-  (** {7 Destructors} *)
-
   val destruct : t -> Sym.t * t list
     (** [destruct a] destructure an application [a] of the form
       [App(f, l)] into [(f, l)]. *)
@@ -119,8 +163,7 @@ module App : sig
 
   val args_of : t -> t list
     (** [sym_of a] returns the argument list [l] of an application
-      [a] of the form [App(_,l)]. *)
-
+      [a] of the form [App(_,l, _)]. *)
 
 end
 
@@ -134,11 +177,17 @@ val is_app : t -> bool
   (** [is_app a] holds iff [a] is of the form [App _]. *)
 
 val is_const : t -> bool
-  (** [is_const a] holds iff [a] is of the form [App(_,[])]. *)
+  (** [is_const a] holds iff [a] is of the form [App(_,[], _)]. *)
 
 val is_pure : Th.t -> t -> bool
   (** [is_pure i a] holds iff all function symbols in [a] are of 
-    theory [i]. *)
+    theory [i] (see {Theory.t}). *)
+
+val is_equal : t -> t -> Three.t
+  (** [is_equal a b] returns
+    - [Three.Yes] if {!Term.eq}[a b] holds,
+    - [Three.No] is [a] and [b] are two distinct constants in some theory [th], and
+    - [Three.X] if none of the above holds. *)
 
 
 (** {6 Comparisons} *)
@@ -168,6 +217,10 @@ val cmp : t -> t -> int
     and the orderings on respective terms in the argument lists [al]
     and [bl]. *)
 
+val compare : t -> t -> int
+  (** Total comparison on term which is designed to be fast.
+    Does not necessarily coincide with {!Term.cmp}. *)
+
 
 val (<<<): t -> t -> bool
   (** [a <<< b] iff [cmp a b <= 0]. *)
@@ -177,13 +230,15 @@ val orient : t * t -> t * t
     and [(a, b)] otherwise. *)
 
 
-val is_equal : t -> t -> Three.t
-
-
 (** {6 Iterators} *)
 
 val fold : (t -> 'a -> 'a) -> t -> 'a -> 'a
-  (** Fold operator on terms. *)
+  (** Fold operator [fold f a e] on terms applies 
+    argument function [f] to all variables in [a]
+    and accumulates the results starting with [e]. 
+    Thus, if {!Term.var_of][a] is of the form [{x1,...,xn}]
+    with the order of variables unspecified,
+    then [fold f a e] reduces to [f x1 (f x2 ... (f xn e))]. *) 
 
 val iter : (t -> unit) -> t -> unit
   (** Iteration operator on terms. *)
@@ -194,31 +249,17 @@ val mapl : (t -> t) -> t list -> t list
 
 (** {6 Predicates} *)
 
-val for_all : (t -> bool) -> t -> bool
-  (** Predicate holds for all subterms. *)
-
 val subterm : t -> t -> bool
   (** [subterm a b] holds if [a] is a subterm of [b]. *)
 
 val occurs : t -> t -> bool
-  (** [occurs x a] holds if term [x] occurs in [a]. *)
+  (** [occurs a b] holds if term [a] occurs in [b]. *)
 
 val assq : t -> (t * 'a) list -> 'a
   (** Association lists for terms. *)
 
 
-(** {6 Pretty-Printing} *)
-
-val pretty : bool ref
-
-val pp : Format.formatter -> t -> unit
-
-val to_string : t -> string
-
-
 (** {6 Sets and maps of terms} *)
-
-type trm = t
 
 module Set2 : (Set.S with type elt = trm * trm)
 
@@ -226,11 +267,11 @@ module Set : (Set.S with type elt = trm)
 
 module Map : (Map.S with type key = trm)
 
-val vars_of : t -> Set.t
+val vars_of : t -> Var.Set.t
   (** Return set of variables. *)
 
 
-
+(** Following to be deprecated. *)
 module Equal : sig
   type t = trm * trm
   val lhs : t -> trm
@@ -243,6 +284,7 @@ module Equal : sig
   val is_pure : Th.t -> t -> bool
 end
 
+(** Following to be deprecated. *)
 module Diseq : sig
   type t = trm * trm
   val lhs : t -> trm
@@ -255,7 +297,7 @@ module Diseq : sig
 end
 
 
-(** {6 Term Substitution} *)
+(** {6 Term Substitutions} *)
 
 type apply = t * t -> t -> t
 type map = (t -> t) -> (t -> t)
@@ -280,10 +322,3 @@ module Subst : sig
   val fold : (trm * trm -> 'a -> 'a) -> t -> 'a -> 'a
 
 end
-
-
-
-(** {6 Hashing} *)
-
-val hash : t -> int
-
