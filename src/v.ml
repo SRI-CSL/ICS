3c34e665152b6462d35ed4d65c791bcd58a13d25
ruess <ruess>
2003-07-29 20:30:10 +0000

    changed to a new representation for pre

--- src/v.ml
+++ src/v.ml
@@ -11,274 +11,330 @@
  * benefit corporation.
  *)
 
-open Term
-
-(* Elements of type [t] represent sets of variable equalities [x = y] such that
-  [x] is syntactically different from [y], that is, {!Term.eq}[ x y] does not hold.
-  Also, these sets are functional in the sense that whenever both [x1 = y] and [x2 = y]
-  are represented, then [x1] equals [x2]. The [find] datastructure is used to represent
-  equalities such as [x = y] by a binding [x |-> (y, rho)], where [rho] is a justification
-  of the equality [x = y]; see also {!Fact.justification}. The [inv] map is used to 
-  memoize the inverse find, that is, [x] is in [inv s y] iff [find] represents [x = y].
-  Finally, [removable] contains internally generated renaming variables that are not 
-  canonical w.r.t to the given set of equalities.  These variables may be garbage 
-  collected. *)
+(** Elements of type [t] represent sets of directed variable equalities [y = x] 
+  such that [x] is less than [y] according to the variable comparison {!Var.cmp}. 
+  These sets are functional in the sense that whenever both [x1 = y] and [x2 = y]
+  are represented, then [x1] equals [x2].  
+  - The [post] datastructure is used to represent equalities such as [x = y] 
+  by a binding [x |-> (y, rho)], where [rho] is a justification of the equality 
+  [x = y]; see also {!Jst.t}. 
+  - The [pre] map is used to memoize the inverse find, that is, [x] is in [pre s y] iff 
+  [x |-> (y, _)] is in [post]. 
+  - Finally, [removable] contains internally generated renaming variables that are
+  not canonical w.r.t to the given set of equalities.  These variables may be 
+  {i garbage collected}. 
+*)
   
+
+(** Switch for enabling/disabling garbage collection of noncanonical, internal
+  variables. *)
+let garbage_collection_enabled = ref true
+				   
+
+(** The [pre] of a canonical term [x] contains all noncanonical terms [y]
+  equivalent with [x].  This set is represented as a tree with
+  terms at the leafs. A term does occur at most once in [pre].  This 
+  enables {!V.merge} to be fast, whereas {!V.restrict} requires a linear 
+  scan of [pre] but at most one node is changed. *)
+module Pre = struct
+
+  type t = 
+    | Empty
+    | Node of Term.t
+    | Union of t * t
+
+  let empty = Empty
+
+  let is_empty = function
+    | Empty -> true
+    | _ -> false
+
+  let singleton a = Node(a)
+
+  let add a p = 
+    match p with
+      | Empty -> Node(a)
+      | _ -> Union(Node(a), p)
+
+  let union p1 p2 = 
+    match p1, p2 with
+      | Empty, _ -> p2
+      | _, Empty -> p1
+      | _ -> Union(p1, p2)
+
+  let rec remove a p =
+    match p with
+      | Empty -> 
+	  Empty
+      | Node(b) -> 
+	  if Term.eq a b then Empty else p
+      | Union(p1, p2) ->
+	  let p1' = remove a p1 in
+	    if p1 == p1' then 
+	      let p2' = remove a p2 in
+		if p2 == p2' then p else
+		  union p1 p2'
+	    else  (* successful removal from [p1] *)
+	      union p1' p2
+	  
+  let fold f p e =
+    let rec loop acc = function
+      | Empty -> acc
+      | Node(b) -> f b acc
+      | Union(p1, p2) -> loop (loop acc p1) p2
+    in
+      loop e p
+
+  let elements p = 
+    let cons a al = a :: al in
+      fold cons p []
+
+  let rec iter f = function
+    | Empty -> ()
+    | Node(b) -> f b
+    | Union(p1, p2) -> iter f p1; iter f p2
+
+  let rec for_all f = function
+    | Empty -> true
+    | Node(b) -> f b
+    | Union(p1, p2) -> for_all f p1 && for_all f p2
+
+  let rec exists f = function
+    | Empty -> false
+    | Node(b) -> f b
+    | Union(p1, p2) -> exists f p1 || exists f p2
+
+end
+
+(** [x |-> (y, rho)] in [post] represents [rho |- x = y].
+  In such a case [x] is in the [pre] of [y]. Furthermore,
+  this data structure is {i maximally compressed} in that
+  whenever [x |-> (y, .)], then there is no [z] such that 
+  [y |-> (z, .)]. The set of removable terms is not 
+  represented as a list as this makes {!V.restrict} more 
+  expensive. *)
 type t = {
-  find : (Term.t * Justification.t) Map.t;
-  inv : Set.t Map.t; 
-  removable: Term.Set.t
+  post : (Term.t * Jst.t) Term.Var.Map.t; 
+  pre : Pre.t Term.Var.Map.t;      
+  removable: Term.Var.Set.t
 }
 
-
-(** {6 Identity} *)
-
-let eq s t = s.find == t.find
-
-
-(** {6 Accessors} *)
-
-let apply s = function
-  | App _ -> raise Not_found           (* only variables in domain. *)
-  | x -> Map.find x s.find
-
-let find s x =
-  let rec loop x (y, rho) =            (* [rho |- x = y] *)
-    try 
-      let (z, sigma) = apply s y in    (* [sigma |- y = z] *)
-	Trace.msg "v'" "find" (y, z) (Pretty.pair Term.pp Term.pp);
-	assert(not(Term.eq y z));
-	let tau = Justification.trans (x, y, z) rho sigma in
-	  loop x (z, tau)
-    with 
-	Not_found -> (y, rho)
-  in
-    loop x (Justification.Eqtrans.id x)
-
-let inv s x = 
-  try
-    (match x with
-       | App _ -> raise Not_found
-       | _ -> Map.find x s.inv)
-  with
-      Not_found -> Term.Set.empty
-
-let removable s = s.removable
-
-let is_canonical s x = 
-  try
-    let (y, _) = apply s x in
-      Term.eq x y
-  with
-      Not_found -> true
-
-
-(** {6 Basic Data Manipulations} *)
-
-let union (x, y, rho) s = 
-  Trace.msg "v" "Union" (x, y) Term.Equal.pp;
-  assert(not(Term.eq x y));
-  {find = Map.add x (y, rho) s.find;
-   inv = Map.add y (Set.add x (inv s y)) s.inv;
-   removable = if Term.Var.is_internal x then Set.add x s.removable else s.removable}
-
-(** Remove a binding [x |-> y] *)
-let remove x s =
-  try
-    let (y, rho) = apply s x in
-      Trace.msg "v" "Remove" (x, y) Term.Equal.pp;
-      let find' = Term.Map.remove x s.find in
-      let invy' = Term.Set.remove x (inv s y) in
-      let inv' = 
-	if Term.Set.is_empty invy' then 
-	  Term.Map.remove y s.inv
-	else 
-	  Term.Map.add y invy' s.inv
-      in
-	{find = find'; 
-	 inv = inv'; 
-	 removable = Term.Set.remove x s.removable}
-  with
-      Not_found -> s
-    
-
-(** For [zi |-> x |-> y |-> ... |-> y'] set [zi |-> y'] and remove [x]. *)
-let restrict x s =
-  assert(Term.Set.mem x s.removable);
-  if is_canonical s x then s else
-    let s' =
-      Term.Set.fold
-	(fun z acc ->
-	   let (z', tau') = find s z in  (* change [z |-> x] to [z |-> z'] *)
-	     union (z, z', tau') (remove z acc))
-	(inv s x) s
-    in
-      remove x s'   (* now, break the link [x |-> y]. *)
- 
-
-(** {6 Variable equality modulo [s]} *)
-
-let is_equal s x y = 
-  let (x', rho) = find s x         (* [rho |- x = x'] *)
-  and (y', sigma) = find s y in 
-    if Term.eq x' y' then          (* [sigma |- y = x'] *)
-      Some(Justification.trans (x, x', y) rho (Justification.sym (x', y) sigma))
-    else 
-      None
-
-
-(** {6 Context Manipulations} *)
+let eq s t = s.post == t.post
 
 let empty = {
-  find = Map.empty;
-  inv = Map.empty;
-  removable = Term.Set.empty
+  post = Term.Var.Map.empty;
+  pre = Term.Var.Map.empty;
+  removable = Term.Var.Set.empty;
 }
 
-let is_empty s = (s.find == Map.empty)
+let is_empty s = (s.post == Term.Var.Map.empty)
 
-(** Garbage collection *)
-let gc f s =
-  Set.fold
-    (fun x s ->
-       if f x then restrict x s else s)
-    s.removable
-    s
-   
-(** Adding a binding [x |-> y] to a context [s]. *)
-let merge e s =                (* [rho |- x = y] *)
-  assert (Fact.Equal.is_var e);
-  Trace.msg "v" "Merge" e Fact.Equal.pp;
-  let e' = Fact.Equal.map (find s) e in
-  let (x', y', rho') = Fact.Equal.destruct e' in
-    if Term.eq x' y' then s else
-      begin
-	Fact.Eqs.push None e';
-	union (x', y', rho') s
-      end 
-	  
+let post s x = Term.Var.Map.find x s.post
 
-(** {6 Functionals} *)
+let pre s x = 
+  let ys = Term.Var.Map.find x s.pre in
+    assert(not(Pre.is_empty ys));
+    ys
 
-(** Starting from the canonical representative [x' = find s x], the
-  function [f] is applied to each [y] in [inv s x'] and the results are
-  accumulated. *)
-let fold s f x =
-  let rec loop y acc =
-    let acc' = f y acc in
-    try
-      Set.fold loop (Map.find y s.inv) acc'
-    with
-	Not_found -> acc'
-  in
-  let (y, _) = find s x in
-    loop y
+(** Canonical representative of equivalence class for [x]. *)
+let find s x = 
+  match x with
+    | Term.Var _ ->
+	(try post s x with Not_found -> Jst.Eqtrans.id x)
+    | _ ->
+	Jst.Eqtrans.id x
 
+(** Totalized [pre]. *)
+let inv s x = 
+  assert(Term.is_var x);
+  try pre s x with Not_found -> Pre.empty
 
-(** Extension of the equivalence class for [x]. *)
-let ext s x = 
-  fold s Set.add x Set.empty
-
-
-(** Iteration. *)
-let iter s f x =
-  let rec loop y =
-    f y;
-    try
-      Set.iter loop (Map.find y s.inv)
-    with
-	Not_found -> ()
-  in
-  let (y, _) = find s x in
-    loop y
-
-
-let exists s p x =
-  let rec loop y =
-    p y || 
-    try
-      Set.exists loop (Map.find y s.inv)
-    with
-	Not_found -> false
-  in
-  let (y, _) = find s x in
-    loop y
-
-
-let for_all s p x =
-  let rec loop y =
-    p y &&
-    try
-      Set.for_all loop (Map.find y s.inv)
-    with
-	Not_found -> true
-  in
-  let (y, _) = find s x in
-    loop y
-
-
-(** Choose an element satisfying some property. *)
-exception Found
-
-let choose s p x =
-  let result = ref (Obj.magic 1) in
-  try
-    iter s 
-      (fun y ->
-	 match p y with
-	   | Some(z) -> 
-	       result := z;
-	       raise Found
-	   | None -> ())
-      x;
-    raise Not_found
-  with
-      Found -> !result
-
-    
-(** {6 Pretty-printing.} *)
-
-let pretty = ref true
-
-(** Representation as [z |-> {x1, ...,xn}] with [z] the
-  canonical variables, and [xi] the variables with [z]
-  as canonical representative. *)
-let to_map s =
-  Map.fold  
-    (fun x (y, rho) acc ->         (* [rho |- x = y] *)
-       let (z, tau) = find s y in  (* [tau |- y = z] *)
-	 try
-	   let zs = Term.Map.find z acc in
-	   let zs' = Term.Set.add x zs in
-	     Term.Map.add z zs' acc
-	 with
-	     Not_found -> 
-	       Term.Map.add z (Term.Set.singleton x) acc)
-    s.find Term.Map.empty
+(** Set of removable variables. *)
+let removable s = s.removable
 
 let to_list s =
-  Map.fold 
-    (fun x xs acc -> 
-       (x, Term.Set.elements xs) :: acc) 
-    (to_map s) []
+  Term.Var.Map.fold
+    (fun _ (y, _) acc ->
+       (y, (Pre.elements (inv s y))) :: acc) 
+    s.post []
 
+let to_equalities s =
+  Term.Var.Map.fold 
+    (fun x (y, rho) acc ->
+       Fact.Equal.make (x, y, rho) :: acc)
+    s.post []
+
+let pp_as_equalities = ref false
+       
 let pp fmt s =
   if not(is_empty s) then
     begin
       Pretty.string fmt "\nv:";
-      if !pretty then
-	let l = to_list s in
-	  Pretty.map Term.pp (Pretty.set Term.pp) fmt l
+      if !pp_as_equalities then
+	Pretty.set Fact.Equal.pp fmt (to_equalities s)
       else 
-	let l = 
-	  Map.fold 
-	    (fun x (y, rho) acc -> (x, (y, rho)) :: acc)
-	    s.find []
-	in
-	  Pretty.map
-	    Term.pp 
-	    (Pretty.pair Term.pp Justification.pp)
-	    fmt 
-	    l
-    end 
+	Pretty.map Term.pp (Pretty.set Term.pp) fmt (to_list s)
+    end  
+
+(** A variable [x] is {i canonical} iff it is not in the domain
+  of the [post] function of [s]. In this case, {!V.find} is the
+  identity. *)
+let is_canonical s x =  
+  assert(Term.is_var x);
+  not(Term.Var.Map.mem x s.post)
+
+(** Merging of two different canonical variables [x] and [y] *)
+let rec merge e s =            
+  assert (Fact.Equal.is_var e);
+  let ((x, y, _) as eql) = Fact.Equal.destruct e in
+    if Term.eq x y then s else
+      begin 
+	Trace.msg "v" "Union" e Fact.Equal.pp;
+	Fact.Eqs.push None e;
+	union s eql
+      end 
+
+(** Merging [x = y] is performed by 
+  - adding [x |-> y] and 
+  - replacing all links [z |-> x] with [z |-> y]. *)
+and union s (x, y, rho) = 
+  assert(not(Term.eq x y));            (* [rho |- x = y] *)
+  assert(is_canonical s x);
+  assert(is_canonical s y);
+  let removable' = 
+    if !garbage_collection_enabled &&
+      Term.Var.is_internal x 
+    then 
+      Term.Var.Set.add x s.removable
+    else 
+      s.removable
+  in   
+  let pre_of_x = inv s x in 
+  let post' = 
+    Pre.fold
+      (fun z ->                         (* [tau |- z = x] *)
+	 let (_, tau) = find s z in
+	 let sigma = Jst.trans z x y tau rho in
+	   Term.Var.Map.add z (y, sigma))
+      pre_of_x
+      (Term.Var.Map.add x (y, rho) s.post)
+  in
+  let pre' = 
+    try
+      let pre_of_y = pre s y in
+      let pre_of_y' = Pre.add x (Pre.union pre_of_y pre_of_x) in
+	Term.Var.Map.add y pre_of_y'
+	  (Term.Var.Map.remove x s.pre)
+    with
+	Not_found -> 
+	  Term.Var.Map.add y (Pre.add x (inv s x))
+	  (Term.Var.Map.remove x s.pre)
+  in
+    {post = post'; 
+     pre = pre'; 
+     removable = removable'}
+      
+      
+(** Remove a binding [x |-> y] *)
+let restrict s x =
+  assert(Term.is_var x);
+  assert(not(is_canonical s x));
+  try
+    let (y, rho) = post s x in
+      Trace.msg "v" "Remove" x Term.pp; 
+      assert(is_canonical s y); 
+      assert(not(Pre.is_empty (inv s y)));
+      let removable' = 
+	Term.Var.Set.remove x s.removable in
+      let post' = 
+	Term.Var.Map.remove x s.post in
+      let pre' =
+	let pre_of_y' = Pre.remove x (inv s y) in
+	  if Pre.is_empty pre_of_y' then
+	    Term.Var.Map.remove y s.pre
+	  else 
+	    Term.Var.Map.add y pre_of_y' s.pre
+      in
+	{post = post'; 
+	 pre = pre'; 
+	 removable = removable'}
+  with
+      Not_found -> s
+
+(** Variable equality modulo [s] *)
+let is_equal s x y = 
+  assert(Term.is_var x);
+  assert(Term.is_var y);
+  let (x', rho) = find s x         (* [rho |- x = x'] *)
+  and (y', sigma) = find s y in 
+    if Term.eq x' y' then          (* [sigma |- y = y'] *)
+      Some(Jst.trans x x' y rho sigma)
+    else 
+      None
+
+(** Garbage collection *)
+let gc f s =
+  let gc1 x s =
+    assert(Term.is_var x);
+    if f x then restrict s x else s
+  in
+  Term.Var.Set.fold gc1 s.removable s
 	  
+(** Extension of the equivalence class for [x] contains
+  all [y] such that [x] and [y] are equal modulo [s].
+  Expensive operation, uses memory linear in the size of the
+  [pre] of [y]. *)
+let ext s x =
+  assert(Term.is_var x);
+  let (y, _) = find s x in
+    Pre.elements (Pre.add y (inv s y))
+
+(** Starting from the canonical representative [x' = find s x], the
+  function [f] is applied to each [y] in [ext s x'] and the results are
+  accumulated. *)
+let fold s f x e = 
+  assert(Term.is_var x);
+  let (y, _) = find s x in
+    Pre.fold f (inv s y) (f y e)
+
+(** Iteration on extension of equivalence class. *)
+let iter s f x =
+  assert(Term.is_var x);
+  let (y, _) = find s x in
+    f y;
+    Pre.iter f (inv s y)
+
+let exists s p x = 
+  assert(Term.is_var x);
+  let (y, _) = find s x in
+    p y || Pre.exists p (inv s y) 
+
+let for_all s p x =  
+  assert(Term.is_var x);
+  let (y, _) = find s x in
+    p y && Pre.for_all p (inv s y) 
+
+exception Found
+
+(** Choose an element satisfying some property. *)
+let choose s p x = 
+  assert(Term.is_var x);
+  let (y, _) = find s x in
+    match p y with
+      | Some(z) -> z
+      | None ->
+	  let result = ref (Obj.magic 1) in
+	    try     
+	      Pre.iter
+		(fun y ->
+		   match p y with
+		     | Some(z) -> 
+			 result := z;
+			 raise Found
+		     | None -> ())
+		(inv s y);
+	      raise Not_found
+	    with
+		Found -> !result
+
+ 
