fac12bd697bf04c087cfebf775ea758993b88705
ruess <ruess>
2004-06-07 19:38:40 +0000

    *** empty log message ***

---
+++ src/icsversion.c
@@ -0,0 +1,38 @@
+#include <stdio.h> 
+#include <string.h>
+#include <caml/mlvalues.h>
+#include <caml/alloc.h>
+#include <caml/callback.h>
+#include <caml/memory.h>
+#include <caml/fail.h>
+
+char* version = VERSION;
+char* arch = ARCHITECTURE;
+char* date = BUILDDATE;
+int debug = DEBUG;
+char* flags = COMPILATION_FLAGS;
+
+void print_version() {
+  CAMLparam0();
+  fprintf(stdout, "ICS %s (%s, %s)", version, arch, date); 
+  if (debug == 1) {
+    fprintf(stdout, "\nDebugging enabled.");
+  };
+  fflush(stdout);
+  CAMLreturn0;
+}
+
+void eprint_version() {
+  CAMLparam0();
+  fprintf(stderr,"ICS %s (%s, %s)", version, arch, date); 
+  if (debug == 1) {
+    fprintf(stderr,"\nDebugging enabled.");
+  };
+  fflush(stderr);
+  CAMLreturn0;
+}
+
+int debug_value () {
+  CAMLparam0();
+  CAMLreturn(Val_int(debug));
+}
