eb3a29cbe0725cde3c68285b384cde970764820b
owre <owre>
2005-06-17 21:21:59 +0000

    removed

--- get-time.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-if grep -e "user" $1 2> /dev/null > /dev/null; then
-		EXEC_TIME=`grep -e "user" $1 | awk '{print $1}'`
-		EXEC_TIME=${EXEC_TIME//user/}
-		echo $EXEC_TIME
-else
-		echo "NOT_AVAILABLE"
-fi
